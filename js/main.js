26a1fca33cfc3ed5cb3c2eca79c8129a6fe07052
Grace <alpha07ster@gmail.com>
2023-05-16 17:46:14 -0400

    adding more files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+
+
 function sendEmail(){
     
     Email.send({
-    Host : "smtp.elasticemail.com",
-    Username : "alpha07ster@gmail.com",
-    Password : "3B26740CC0F4C6A25F2893EF40C2751C20E6",
-    To : "alpha07ster@gmail.com",
-    From : "alpha07ster@gmail.com",
-    Subject : "New Contact Form Enquiry",
-    Body : "And this is the body",
+        Host : "smtp.elasticemail.com",
+        Username : "alpha07ster@gmail.com",
+        Password : "3B26740CC0F4C6A25F2893EF40C2751C20E6",
+        To : "alpha07ster@gmail.com",
+        From : "alpha07ster@gmail.com",
+        Subject : "New Contact Form Enquiry",
+        Body : "And this the message",
     
-}).then(
-message => alert(message)
+    }).then(
+    message => alert("Thank you! Your message has been sent")
 );
 
 }
